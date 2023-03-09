8cff757fd09a80f10c6c90a21f7e2123600aa724
Michał Krasnodębski <m.krasnodebski2000@gmail.com>
2023-03-09 23:03:10 +0100

    add field and constructor to casino file

--- src/casino/Casino.java
+++ src/casino/Casino.java
@@ -1,4 +1,14 @@
 package casino;
 
 public class Casino implements Playable {
+    private LinkedList<Integer> players;
+
+    public Casino(LinkedList<Integer> players){
+        this.players = players;
+    }
+
+    public Casino(){
+        this.players = new LinkedList<Integer>();
+    }
+
 }
